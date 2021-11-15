67fdee0f244bbe3d1c5cb216f468f54c2d76dc15
LluisNoguera <lluisnoguera@paucasesnovescifp.cat>
2021-11-15 17:43:41 +0100

    Initializing repo..

---
+++ src/main/java/spdvi/Constants.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.spdvi;
+
+import java.util.List;
+
+/**
+ *
+ * @author Lluis
+ */
+public class Constants {
+    public static final String APP_FOLDER = System.getProperty("user.home") + "\\AppData\\Local\\OpusList\\";
+    public static final String IMAGES_FOLDER = System.getProperty("user.home") + "\\AppData\\Local\\OpusList\\images\\";
+    public static final String NO_IMAGE = "/spdvi/noImage.png";
+    public static final java.lang.reflect.Type LIST_OF_OBRA_TYPE = new com.google.gson.reflect.TypeToken<List<Obra>>() {}.getType();
+    public static final String JSON_DATA_FILE = System.getProperty("user.home") + "\\AppData\\Local\\OpusList\\data\\obres.json";
+    public static final int DEFAULT_PROFILE_IMAGE_WIDTH = 96;
+    public static final int DEFAULT_PROFILE_IMAGE_HEIGHT = 96;
+    public static final String DEFAULT_PROFILE_IMAGE_TYPE = "jpg";
+}
